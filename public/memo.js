31d82f73ebf49ec7ce3edff697202b21960591a6
Vadym Nemchenko <westa.nvv@gmail.com>
2024-09-25 15:40:53 +0300

    Added husky, lint-staged

--- public/memo.js
+++ public/memo.js
@@ -91,3 +91,126 @@ function add(a, b) {
 // // console.log(obj1.arr2 ? obj1.arr2[100] : 'none');
 // // console.log(obj1.arr2 && obj1.arr2[100]);
 // console.log(obj1.arr2?.[100]);
+
+// ----------------
+const getFromAPI = (userId, callback) => {
+  fetch('http://eventregistry.org/api/v1/article/getArticles?apiKey=f234c7ef-33a4-47c4-9f38-0d31451a5824')
+    .then(data => data.json())
+    .then(data => {
+      callback({id: 1, name: 'ubaser'});
+    });
+};
+
+const openModal = (data, okCallback, cancelCallback) => {
+  okCallback(data);
+};
+
+const sendToAPI = (data, callback) => {
+  callback(data);
+};
+
+// getFromAPI(1000, data => {
+//   openModal(
+//     data,
+//     modalData => {
+//       sendToAPI(modalData, sendData => {
+//         console.log('done', sendData);
+//       });
+//     },
+//     () => {
+//       console.log('cancel');
+//     }
+//   );
+// });
+
+const getFromAPI2 = userId => {
+  return fetch('http://eventregistry.org/api/v1/article/getArticles?apiKey=f234c7ef-33a4-47c4-9f38-0d31451a5824')
+    .then(data => data.json())
+    .then(data => {
+      return {id: 1, name: 'ubaser'};
+    });
+};
+
+const openModal2 = data => {
+  return new Promise((resolve, reject) => {
+    //
+    resolve(data);
+  });
+};
+
+const sendToAPI2 = data => {
+  return new Promise((resolve, reject) => {
+    // send
+    resolve(data);
+  });
+};
+
+// console.log(1);
+//
+// getFromAPI2(1000)
+//   .then(data => {
+//     return openModal2(data);
+//   })
+//   .then(data => {
+//     return sendToAPI2(data);
+//   })
+//   .then(data => {
+//     console.log('done', data);
+//   })
+//   .finally(() => {
+//     // close modal
+//   });
+//
+// console.log(2);
+
+//
+const sleep = time => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('wake up');
+      resolve();
+    }, time);
+  });
+};
+
+// // sleep(2000, () => {
+// //   sleep(2000, () => {
+// //     sleep(2000, () => {
+// //       sleep(2000, () => {
+// //         sleep(2000, () => {
+// //           sleep(2000, () => {
+// //             console.log('prepare');
+// //           });
+// //         });
+// //       });
+// //     });
+// //   });
+// // });
+//
+
+const test2 = async () => {
+  const data = await getFromAPI2(1000);
+  const dataModal = await openModal2(data);
+  const result = await sendToAPI2(dataModal);
+  console.log('done', result);
+};
+
+// test2();
+
+(async () => {
+  await sleep(2000);
+
+  async function test() {
+    console.log(1);
+
+    await sleep(2000);
+    await sleep(2000);
+    await sleep(2000);
+
+    const [resp1, resp2] = await Promise.all([getFromAPI2(1000), getFromAPI2(11)]);
+
+    console.log(2);
+  }
+})();
+
+(() => {})();
